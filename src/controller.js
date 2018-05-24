6f952e3d086ce62108215e60ca488a1580c4b79f
Josh Perry <josh.perry@roostermornings.com>
2018-05-24 12:09:37 +0200

    Set up PubSub class and basic methods

--- src/controller.js
+++ src/controller.js
@@ -5,3 +5,17 @@
  *  Controller: Central controller to control program flow
  *  Author: All
  */
+
+const PubSubInterface = require('./pubsub.js');
+
+const spheroType = {
+ 	"leader": 		0,
+ 	"follower": 	1
+}
+
+// Set the sphero type here
+const sphero = spheroType.follower;
+
+// Create a pubsub interface 
+const pubsub = new PubSubInterface(sphero);
+
---
+++ src/pubsub.js
@@ -0,0 +1,125 @@
+/*
+ *  OfferZen Make Day 24 May 2018
+ *  Team: Brandon Piner, Josh Perry, Jason Hady
+ *
+ *  PubSub: Network layer to interface with the AWS console
+ *  Author: Peza
+ *
+ *  Protocol: {Team-ID/Sphero-Type/command}
+ */
+
+const awsIot = require('aws-iot-device-sdk');
+const moment = require('moment'); // for DateTime formatting
+const username = 'peza8';
+
+// JS typedef
+const spheroType = {
+  "leader":     0,
+  "follower":   1
+}
+
+// Class level constants
+const subTopics = {0: ['makers/challenge/clues'],
+                   1: ['sheep/follower/test']};
+
+class PubSubInterface {
+  
+  // Initial connection
+  constructor(spheroType) {
+    // Used for differentiation between lead and follow
+    this.spheroType = spheroType;
+    this.initDevice();
+  }
+
+  initDevice() {
+    console.log(`PUBSUB: Setting up Jarvis`);
+    var self = this;
+
+    if (this.spheroType === spheroType.follower) {
+      this.device = awsIot.device({
+         keyPath: 'certs_follower/1fa768eb95-private.pem.key',
+        certPath: 'certs_follower/1fa768eb95-certificate.pem.crt',
+          caPath: 'certs_follower/root-ca.pem',
+        clientId: `${username}-subscribe`,
+            host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com' 
+      });
+    } 
+
+    else {
+      // Todo
+      this.device = awsIot.device({
+         keyPath: '',
+        certPath: '',
+          caPath: '',
+        clientId: ``,
+            host: '' 
+      });
+    }
+
+    // Set up listeners
+    this.device.on('connect', function() {
+      console.log('PUBSUB: Subscriber client connected to AWS IoT cloud.');
+
+      // Set up subscribe events
+      const topics = subTopics[self.spheroType];
+      for (var index in topics){
+        self.device.subscribe(topics[index]);
+        console.log(`PUBSUB: Subscribed to ~${topics[index]}`);
+      }
+    });
+
+    this.device.on('error', error => {
+      // Failure to connect
+      console.log(`PUBSUB: Eish, struggling to connect this bugger!`);
+      console.log(error);
+    });
+
+    this.device.on('message', (topic, payload) => {
+      self.handleNewMsg(topic, payload);
+    });
+  }
+
+
+
+  handleNewMsg(topic, payload) {
+    // Get the message from json string
+    const message = JSON.parse(payload.toString());
+
+    switch (topic) {
+      case 'sheep/follower/test':
+        console.log('PUBSUB: Got a message from houston!');
+        break;
+         
+      default:
+        console.log(`Message received on topic "${topic}"`)
+    }
+  }
+
+  /* ----------------------------------------------------------
+   *
+   *                      Publishing Section
+   *
+     ---------------------------------------------------------- */
+
+    publishMsg(topic, message, token){
+      console.log(`PUBSUB: Publishing ${message} to topic ${topic}`);
+
+      /*
+      this.device.publish(topic, JSON.stringify({
+      });
+      */
+    }
+
+
+}
+
+module.exports = PubSubInterface;
+
+
+
+  
+
+  
+
+
+
